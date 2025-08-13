71a11cd4519449c6d3ddf891f97b3385b93776fd
al-imam <alimam01828@gmail.com>
2025-08-13 21:32:48 +0600

    feat: update access token creation to use dynamic permissions

--- src/controllers/access.controller.ts
+++ src/controllers/access.controller.ts
@@ -4,11 +4,16 @@ import { env } from "~/env";
 import { UnauthorizedError, ZodValidationError } from "~/lib/http";
 import { sign } from "~/lib/jwt";
 
+export const permissions = ["read", "write", "delete"] as const;
+export type Permissions = (typeof permissions)[number];
+
 const TokenRequestSchema = z.object({
   expiresIn: z
     .string()
     .regex(/^\d+[smhdy]$/, "expiresIn must be a valid duration")
     .default("1y"),
+
+  permissions: z.array(z.enum(permissions)).default(["read", "write"]),
 });
 
 export const keys = {
@@ -16,21 +21,18 @@ export const keys = {
   cookie: "__media_access_token__",
 } as const;
 
-export const permissions = ["read", "write", "delete"] as const;
-export type Permissions = (typeof permissions)[number];
-
 export async function createAccessToken(req: Request, res: Response) {
   const secret = req.headers["x-secret"];
   if (secret !== env.SECRET_KEY) throw new UnauthorizedError("Permission denied");
 
   const result = TokenRequestSchema.safeParse(req.body);
   if (!result.success) throw new ZodValidationError(result.error);
-  const token = await sign(["write", "read"], { expiresIn: result.data.expiresIn });
+  const token = await sign(result.data.permissions, { expiresIn: result.data.expiresIn });
 
   res.json({
     token,
     expiresIn: result.data.expiresIn,
-    permissions: ["write", "read"],
+    permissions: result.data.permissions,
     keys,
   });
 }
